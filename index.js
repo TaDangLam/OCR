43a21f7d76b31b3441af801611c82bbe3212341f
Tạ Đăng Lâm <tadanglam93@gmail.com>
2025-09-29 16:29:23 +0700

    refactor: enable ApollorServer with schema and resolvers

--- index.js
+++ index.js
@@ -2,19 +2,17 @@ import express from "express";
 import { ApolloServer } from "apollo-server-express";
 import { connectDB } from "./src/libs/prisma.js";
 
-
 // Load schema & resolvers
-// import typeDefs from "./src/schema/schema.js";
-// import resolvers from "./src/resolver/resolver.js";
+import typeDefs from "./src/schema/schema.js";
+import resolvers from "./src/resolvers/resolver.js";
 
 const startServer = async () => { 
     await connectDB();
 
     const app = express();
-    // const server = new ApolloServer({ typeDefs, resolvers });
-
-    // await server.start();
-    // server.applyMiddleware({ app });
+    const server = new ApolloServer({ typeDefs, resolvers });
+    await server.start();               // cần gọi start() trước khi applyMiddleware
+    server.applyMiddleware({ app });    // gắn vào app với route /graphql
 
     app.listen(4000, () => {
         console.log("Server is running on http://localhost:4000/graphql");
