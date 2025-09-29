28c3f379b6bc950fadd0148c35e81a765c239e78
Tạ Đăng Lâm <tadanglam93@gmail.com>
2025-09-29 14:37:04 +0700

    feature: add schema, resolver and service

--- src/service/type/type-service.js
+++ src/service/type/type-service.js
@@ -1,6 +1,61 @@
+import prisma from './../../libs/prisma.js';
 
 const typeService = {
-
+    getAllType: async () => {
+        try {
+            const Types = await prisma.type.findMany();
+            return Types;
+        } catch (err) {
+            console.error(err);
+            throw new Error("Failed to fetch types!");
+        }
+    },
+    createType: async(name) => {
+        try {
+            const newType = await prisma.type.create({
+                data: { name }
+            });
+            return newType;
+        } catch (err) {
+            console.error(err);
+            throw new Error("Failed to add type!");
+        }    
+    },
+    updateType: async(id, name) => {
+        try {
+            const existType = await prisma.type.findUnique({ where: { id }});
+            if(!existType) {
+                throw new Error("Type is not exist!");
+            }
+            const updateType = await prisma.type.update({
+                where: { id },
+                data: { name: name ?? existType.name }
+            });
+            return updateType;
+        } catch (err) {
+            console.error(err);
+            throw new Error("Failed to update types!");
+        }
+    },
+    deleteType: async(id) => {
+        try {
+            const existType = await prisma.type.findUnique({ where: { id }});
+            if(!existType) {
+                throw new Error("Type is not exist!");
+            }
+            await prisma.type.update({
+                where: { id },
+                data: { isDelete: true }
+            });
+            return {
+                success: "OK",
+                messsage: "Delete is Successfully!"
+            }
+        } catch (err) {
+            console.error(err);
+            throw new Error("Failed to update types!");
+        }
+    },
 };
 
 export default typeService;
