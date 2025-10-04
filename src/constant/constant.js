993f3295c3e1c2bafe3fcd9b4c23d349cdf8f9eb
Tạ Đăng Lâm <tadanglam93@gmail.com>
2025-10-04 16:04:00 +0700

    feature: add constant for all service

---
+++ src/constant/constant.js
@@ -0,0 +1,18 @@
+export const fileInclude = {
+    type: true,
+    user: true,
+    ocrFields: true,
+    ocr: true
+}
+
+export const userInclude = {
+
+}
+
+export const typeInclude = {
+    files: true,
+}
+
+export const ocrInclude = {
+
+}
