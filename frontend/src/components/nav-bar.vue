ebfadd58890dba19100f716708763096c52ac9f9
Ta Dang Lam <tadanglam93@gmail.com>
2025-10-12 18:02:58 +0700

    Chore(frontend): adjust style css for navbar component

--- frontend/src/components/nav-bar.vue
+++ frontend/src/components/nav-bar.vue
@@ -1,11 +1,22 @@
 <template>
-    <div>
-        is Navbar
+    <div class="flex flex-col gap-4 px-3">
+        <div class="flex items-center justify-center gap-5 py-1">
+            <button type="button" class="py-1 px-5 bg-gray-300 cursor-pointer hover:bg-gray-400 hover:text-white rounded-lg">Upload Template</button>
+            <button type="button" class="py-1 px-5 bg-gray-300 cursor-pointer hover:bg-gray-400 hover:text-white rounded-lg">Upload Files</button>
+            <button type="button" class="py-1 px-5 bg-gray-300 cursor-pointer hover:bg-gray-400 hover:text-white rounded-lg">Add OCR Fields</button>
+            <button type="button" class="py-1 px-5 bg-gray-300 cursor-pointer hover:bg-gray-400 hover:text-white rounded-lg">View Results</button>
+            <button type="button" class="py-1 px-5 bg-gray-300 cursor-pointer hover:bg-gray-400 hover:text-white rounded-lg">Run OCR</button>
+        </div>
+        <div class="flex items-center justify-center gap-5">
+            <button type="button" class="py-1 px-5 bg-gray-300 cursor-pointer hover:bg-gray-400 hover:text-white rounded-lg">Edit OCR Fields</button>
+            <button type="button" class="py-1 px-5 bg-gray-300 cursor-pointer hover:bg-red-400 hover:text-white rounded-lg">Delete OCR Fields</button>
+            <button type="button" class="py-1 px-5 bg-gray-300 cursor-pointer hover:bg-gray-400 hover:text-white rounded-lg">Profile</button>
+        </div>
     </div>
 </template>
 
 <script setup>
-    // defineOptions({ name: 'NavBar' })
+
 </script>
 
 <style scoped>
