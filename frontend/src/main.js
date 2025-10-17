f22320e916ea8ab1a4df3fc4df62d6019de08ff6
Tạ Đăng Lâm <tadanglam93@gmail.com>
2025-10-17 14:47:57 +0700

    chore(frontend): install GraphQL/Apollo dependencies

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -1,13 +1,15 @@
 import { createApp } from '@/libs/vue-export.js';
 import createPinia from '@/libs/pinia.js';
+import App from '@/App.vue';
+import router from '@/router/index.js';
+import '@/styles/main.css';
+import { provideApollo } from '@/libs/apollo-client.js';
 
-import App from '@/App.vue'
-import router from '@/router/index.js'
-import '@/styles/main.css'
+const app = createApp(App);
 
-const app = createApp(App)
+app.use(createPinia());
+app.use(router);
 
-app.use(createPinia())
-app.use(router)
+provideApollo(app);
 
-app.mount('#app')
+app.mount('#app');
