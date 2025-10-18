c09e889a58031bcdeb474857726c5f4d9aaf49d3
Tạ Đăng Lâm <tadanglam93@gmail.com>
2025-10-18 21:47:17 +0700

    fix(server): remove model Type and add field typeName for File Model

--- server/src/resolvers/resolver.js
+++ server/src/resolvers/resolver.js
@@ -1,13 +1,11 @@
 import { mergeResolvers } from "@graphql-tools/merge";
 
-import typeResolvers from './type/type-resolver.js';
 import ocrResolvers from './ocr/ocr-resolver.js';
 import fileResolvers from './file/file-resolver.js';
 import userResolvers from './user/user-resolver.js';
 
 
 const resolvers = mergeResolvers([
-    typeResolvers,
     ocrResolvers,
     fileResolvers,
     userResolvers
--- server/src/schema/schema.js
+++ server/src/schema/schema.js
@@ -3,7 +3,6 @@ import gql from "./../libs/graphql-tag.js";
 
 import fileSchema from "./file/file-schema.js";
 import ocrSchema from "./ocr/ocr-schema.js";
-import typeSchema from "./type/type-schema.js";
 import userSchema from "./user/user-schema.js";
 
 const typeDefs = mergeTypeDefs([
@@ -13,7 +12,6 @@ const typeDefs = mergeTypeDefs([
   `,
   fileSchema,
   ocrSchema,
-  typeSchema,
   userSchema
 ]);
 
