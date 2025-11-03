f98ef6949654fbc2b90ef215a48cff84683beff9
TaDangLam <tadanglam93@gmail.com>
2025-11-03 13:53:31 +0700

    feature(frontend): Emit upload events from FilePreview to HomePage and Navbar to HomePage

--- frontend/src/views/home/home-page.vue
+++ frontend/src/views/home/home-page.vue
@@ -1,30 +1,48 @@
 <template>
-  <div class="flex flex-col gap-3 px-25 pt-10 min-h-screen">
-    <div class="gap-10 w-full bg-gray-100 rounded-lg p-3">
-      <Navbar @update-template="templateFileLocal = $event" />
-    </div>
-    <div class="flex gap-3 w-full">
-      <div class="bg-blue-100 w-1/2 p-3 rounded-lg"><FileReview :templateFileLocal="templateFileLocal" /></div>
-      <div class="bg-blue-100 w-1/2 p-3 rounded-lg"><OcrResult /></div>
-    </div>
-  </div>
+	<div class="flex flex-col gap-3 px-25 pt-10 min-h-screen">
+		<div class="gap-10 w-full bg-gray-100 rounded-lg p-3">
+			<Navbar 
+				@update-template="templateFileLocal = $event"
+				@update-files="handleUploadFiles"
+				:can-upload-files="canUploadFiles"
+			/>
+		</div>
+		<div class="flex gap-3 w-full">
+		<div class="bg-blue-100 w-1/2 p-3 rounded-lg">
+			<FileReview 
+				@can-upload-files="handleCanUploadFiles"
+				:templateFileLocal="templateFileLocal"
+				:uploadFiles="uploadFiles"
+			/>
+		</div>
+
+		<div class="bg-blue-100 w-1/2 p-3 rounded-lg">
+			<OcrResult />
+		</div>
+
+		</div>
+	</div>
 </template>
 
 <script setup>
-  import { ref } from '@/libs/vue-export.js';
-  import Navbar from '@/components/nav-bar.vue';
-  import FileReview from '@/components/file-preview.vue';
-  import OcrResult from '@/components/ocr-result.vue';
+	import { ref } from '@/libs/vue-export.js';
+	import Navbar from '@/components/nav-bar.vue';
+	import FileReview from '@/components/file-preview.vue';
+	import OcrResult from '@/components/ocr-result.vue';
 
-  const templateFileLocal = ref(null);
+	const templateFileLocal = ref(null);
+	const canUploadFiles = ref(false);
+	// const canAddOCRFields = ref(false);
+	// const canViewResults = ref(false);
+	const uploadFiles = ref([]);
+	
+	const handleCanUploadFiles = (event) => {
+		canUploadFiles.value = event;
+	}
 
-  // watch(templateFileLocal, (newVal, oldVal) => {
-  //   console.log('File mới được chọn:', newVal);
-  //   console.log('File cũ được chọn:', oldVal);
-  // })
-  // const confirmTemplate = ref(null);
-  
-  // const childFiles = ref([]);
+	const handleUploadFiles = (event) => {
+		uploadFiles.value = event;
+	}
 </script>
 
 <style scoped></style>
